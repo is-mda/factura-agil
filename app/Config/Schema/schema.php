4a21b83c69505cb9af69ca482b835a501fe1cf7a
antoniosdv <asanchez@avallain.com>
2014-04-24 11:29:25 +0100

    schema update

--- app/Config/Schema/schema.php
+++ app/Config/Schema/schema.php
@@ -123,6 +123,20 @@ class AppSchema extends CakeSchema {
 		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
 	);
 
+	public $orders = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+		'document_id' => array('type' => 'integer', 'null' => false, 'default' => null),
+		'gross_amount' => array('type' => 'float', 'null' => false, 'default' => null),
+		'delivery_address_id' => array('type' => 'integer', 'null' => false, 'default' => null),
+		'estimated_delivery_date' => array('type' => 'date', 'null' => false, 'default' => null),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+
 	public $products = array(
 		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
 		'code' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 13, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
